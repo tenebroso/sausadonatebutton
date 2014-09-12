089c90938a9c575d7a5a9777b11bbad5be156d37
Jon Bukiewicz <jonathan@tenebroso.net>
2014-09-12 12:35:34 -0500

    Cleanup formatting a bit

--- js/scripts.js
+++ js/scripts.js
@@ -4,50 +4,50 @@ var $donateDivInactive = $('.donateBtn.inactive'),
 	$donateMenu = $('.donateMenu'),
 	$donateOptions = $('.donateOptions'),
 	$donateCloseBtn = $('.donateBtn .close'),
-    $donateInput = $('#amount');
+	$donateInput = $('#amount');
 
 $donateInput.keyup(function(event){
-    if(event.keyCode == 13){
-        $('#amount_form').submit();
-    }
+	if(event.keyCode == 13){
+		$('#amount_form').submit();
+	}
 });
 
 
 $donateDivInactive.hover(
-  function () {
-    $donateMenu.transition({ y: '224px' });
-  },
-  function () {
-    $donateMenu.transition({ y: '-224px' });
-  }
+	function () {
+		$donateMenu.transition({ y: '224px' });
+	},
+	function () {
+		$donateMenu.transition({ y: '-224px' });
+	}
 );
 
 $donateBtnInactive.click(
-  function () {
-    $donateMenu.transition({ y: '-224px' });
-    $donateOptions.transition({ y: '224px' });
-    $donateCloseBtn.css('display','block').transition({opacity:100});
-    $('.donateBtn').removeClass('inactive').addClass('active');
-  }
+	function () {
+		$donateMenu.transition({ y: '-224px' });
+		$donateOptions.transition({ y: '224px' });
+		$donateCloseBtn.css('display','block').transition({opacity:100});
+		$('.donateBtn').removeClass('inactive').addClass('active');
+	}
 );
 
 $donateCloseBtn.click(
-  function() { 
-    $donateOptions.transition({y:'-224px'});
-    $donateCloseBtn.css('display','none').transition({opacity:0});
-    $('.donateBtn').removeClass('active').addClass('inactive');
-  }
+	function() { 
+		$donateOptions.transition({y:'-224px'});
+		$donateCloseBtn.css('display','none').transition({opacity:0});
+		$('.donateBtn').removeClass('active').addClass('inactive');
+	}
 );
 
 $('#q_btn_1').click(function() {
-  if( !$('#national_query').val() ) {
-     var url = "http://satruck.org";
-      $(location).attr('href',url);
-  } else {
-    $(this).parent().parent('form').submit();
-  }
+	if( !$('#national_query').val() ) {
+		var url = "http://satruck.org";
+		$(location).attr('href',url);
+	} else {
+		$(this).parent().parent('form').submit();
+	}
 });
 
 $('#q_btn_2').click(function() {
-  $(this).parent().parent('form').submit();
+	$(this).parent().parent('form').submit();
 });
